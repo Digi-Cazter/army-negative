6a372f0645c881fe9877810fef30b95e85fa81e1
Donavan White <donavan@efusionpro.com>
2013-07-02 13:58:44 -0600

    Testing on Rails 4

--- lib/army-negative/version.rb
+++ lib/army-negative/version.rb
@@ -1,8 +1,8 @@
 module Army
   module Negative
     module Version
-      MAJOR  = 3
-      MINOR  = 2
+      MAJOR  = 4 
+      MINOR  = 0
       TINY   = 0
       STRING = [MAJOR, MINOR, TINY].join(".")
     end
